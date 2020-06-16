1782bc0de045c502ddddf31acfd8fa20e1e3c6f0
Zibilyonik <antagonistpotato@gmail.com>
2020-06-16 19:50:17 +0200

    friend request method added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,8 @@ class User < ApplicationRecord
   def friend?(user)
     friends.include?(user)
   end
+  def friend_request(user)
+    return "Error: User already is a friend or has a pending request from you" if current_user.friend?(user)
+    friends << user
+
 end
