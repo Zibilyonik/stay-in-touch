c3b63c7b162050746ef497edf30b20efc1c22be2
Zibilyonik <antagonistpotato@gmail.com>
2020-06-22 22:34:16 +0200

    unnecessary migration file deleted

--- db/migrate/20200622192418_set_primary_key_for_friendships.rb
+++
@@ -1,8 +0,0 @@
-class SetPrimaryKeyForFriendships < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :friendships, :user_id, false
-    change_column_null :friendships, :friend_id, false
-    execute('ALTER TABLE friendships ADD PRIMARY KEY (user_id);')
-    execute('ALTER TABLE friendships ADD PRIMARY KEY (friend_id);')
-  end
-end
